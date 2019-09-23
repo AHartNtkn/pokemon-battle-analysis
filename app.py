f3451954f85294d5507cf97ea917a47a7a127553
AHartNtkn <soobtoob@gmail.com>
2019-09-23 09:44:25 -0700

    change app title

--- app.py
+++ app.py
@@ -54,5 +54,5 @@ meta_tags=[
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets, meta_tags=meta_tags)
 app.config.suppress_callback_exceptions = True
-app.title = 'YOUR APP NAME' # appears in browser title bar
+app.title = 'Predicting the Winner of Pokemon Battles' # appears in browser title bar
 server = app.server
